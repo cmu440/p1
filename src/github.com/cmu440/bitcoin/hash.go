2278b8e914399a16e748dbc1a216ab058f372404
Alex Lockwood <alexjlockwood@gmail.com>
2014-02-25 16:42:51 -0500

    Fixed compile-time error in hash.go

--- src/github.com/cmu440/bitcoin/hash.go
+++ src/github.com/cmu440/bitcoin/hash.go
@@ -9,7 +9,7 @@ import (
 // Hash concatentates a message and a nonce and generates a hash value.
 // Only miners should ever need to call this method.
 func Hash(msg string, nonce uint64) uint64 {
-	hasher = sha256.New()
+	hasher := sha256.New()
 	hasher.Write([]byte(fmt.Sprintf("%s %d", msg, nonce)))
 	return binary.BigEndian.Uint64(hasher.Sum(nil))
 }
