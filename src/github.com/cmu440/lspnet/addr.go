1fd57a77caf6d5908c01ba93bfd2983719ddf73f
Alex Lockwood <alexjlockwood@gmail.com>
2014-03-09 00:47:48 -0500

    Update addr.go

--- src/github.com/cmu440/lspnet/addr.go
+++ src/github.com/cmu440/lspnet/addr.go
@@ -10,5 +10,5 @@ type UDPAddr struct {
 func (a *UDPAddr) String() string { return a.naddr.String() }
 
 func (a *UDPAddr) toNet() *net.UDPAddr {
-	return &net.UDPAddr{a.naddr.IP, a.naddr.Port, a.naddr.Zone}
+	return &net.UDPAddr{IP: a.naddr.IP, Port: a.naddr.Port, Zone: a.naddr.Zone}
 }
